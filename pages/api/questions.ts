db27b1b0868e291f8b4a5c23298a48fd1ada81fb
Ryan <builder@ryancallaghan.com.au>
2021-05-13 15:43:35 +0200

    js to ts

--- pages/api/questions.ts
+++ pages/api/questions.ts
@@ -1,6 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { getQuestionList } from "domain/questions/index";
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const questions = await getQuestionList();
   res.status(200).json(questions);
 };
