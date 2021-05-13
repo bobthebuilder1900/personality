c7d520d424443655e5cdad28e9ae3ecb7f662805
Ryan <builder@ryancallaghan.com.au>
2021-05-13 19:29:02 +0200

    Introduction tests

--- components/Introduction/index.spec.tsx
+++ components/Introduction/index.spec.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-// Using render and screen from test-utils.js instead of
-// @testing-library/react
 import { render, screen } from "utils/testing/test-utils";
 
 import { Introduction } from ".";
 
 describe("Introduction", () => {
-  it("should render the challenge text", () => {
+  beforeEach(() => {
     render(<Introduction />);
-
+  });
+  it("should render the teamway logo", () => {
+    const logo = screen.getByAltText("teamway.io");
+    expect(logo).toBeInTheDocument();
+  });
+  it("should render the challenge text", () => {
     const paragraph = screen.getByText(
       "The goal is to Build a simple personality test application, that takes 3-5 different questions, maps them into a score and produces a personality trait of either Introvert or Extrovert."
     );
-
     expect(paragraph).toBeInTheDocument();
   });
 });
